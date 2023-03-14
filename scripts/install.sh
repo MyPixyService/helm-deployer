acb4a6433e7f7bcffa03b8668bafd2fa06b25f45
MarcZierle <online@marczierle.com>
2023-03-14 17:25:09 +0100

    add install script

---
+++ scripts/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Check if the script is being run as root
+if [ "$EUID" -ne 0 ]; then
+    echo "This script must be run with sudo." >&2
+    echo "You will be prompted for your password." >&2
+    sudo "$0" "$@"
+    exit $?
+fi
+
+# Your script commands go here
+echo "Running with root privileges..."
+
+echo "Downloading latest binary..."
+wget -q --show-progress -O helm-deployer https://github.com/MyPixyService/helm-deployer/releases/download/v0.1.0-alpha/linux-helm-deployer-v0.1.0-alpha
+
+echo "Moving binary to installation directory..."
+mv helm-deployer /usr/local/bin/
+
+echo "Adding execution bit..."
+chmod +x /usr/local/bin/helm-deployer
+
+echo ""
+echo "Helm Deployer has successfully been installed on you system."
+echo "Usage: helm-deployer -f <inputfile.yaml> -k <kubeconfig> [-c <releaseName>] [-uninstall]"
+echo "For more information visit: https://github.com/MyPixyService/helm-deployer"
